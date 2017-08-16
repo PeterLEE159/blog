70fd941608876c1863aeaa3009361a2f3f43ecbc
peter13795@gmail.com <peterlee13795@gmail.com>
2017-08-16 21:39:55 +0900

    asd

--- js/main.js
+++ js/main.js
@@ -1,46 +1,30 @@
 $(function() {
-	$(function() {
-		if($(document).height() <= $(window).height()) {
-			$('footer').addClass('navbar-fixed-bottom');
+
+	var clickEvent = false;
+	$('#blogCarousel').carousel({	// carousel 애니메이션
+		interval: 4000				// carousel 4초 주기로 이동
+	}).on('click', '.list-group li', function() {		// 각 리스트 아이템 클릭 시
+		clickEvent = true;								// 클릭 boolean = true
+		$('.list-group list').removeClass('active');	// 모든 item active 지우기
+		$(this).addClass('active');						// 클릭된 item만 active 추가
+	}).on('slid.bs.carousel', function(e) {				// carousel 이 끝까지 이동했을 때
+		if(clickEvent == false) {						// carousel이 끝난순간 클릭을 안했다면
+			var count = $('.list-group').children().length - 1;			// active를 걸 객체를 제외한 나머지 item 갯수
+			var current = $('.list-group li.active');					// 현재 활성화 된 item
+			current.removeClass('active').next().addClass('active');	// 현재 활성화 되어있는 item의 active를 지우고 다음 item에 active 추가
+			var iId = parseInt(current.data('slide-to'));				// active가 지워진 item의 slide-to 가 가리키는 item의 번호를 구함
+			if(count == iId) {											// item번호가 active걸 객체를 제외한 count와 동일하다면
+				$('.list-group li').first().addClass('active');
+			}
 		}
-		$table = $('#sort-table');
-
-		if($table.length) {
-			$table.tablesorter({
-				sortList: [[0, 0], [0, 1]]
-			});
-		}
-
-		// input에 무언가가 입력되었을 때 이벤트를 발동한다 (keyup)
-		$('.search-input').keyup(function() {
-			
-			// input에 입력된 걸 변수화한다
-			// $.trim($input.val()).toLowerCase(), val()는 input의 value attribute의 내용이다.
-			$searchKey = $(this).val().trim().toLowerCase();
-			
-			// 테이블에 있는 내용을 변수화한다
-			$rows = $('table tr');
-			console.log($rows.length);
-			$rows.show().filter(function() {
-				// $row.text(), text()는 html의 컨텐츠이다.
-				$trText = $(this).text().trim();
-				// 입력된 내용과 테이블 내용을 비교 후 검색내용이 일부라도 일치하는 점이 없다면 테이블내의 row를 감춘다
-				return $trText.indexOf($searchKey) == -1; 
-			}).hide();
-		});
-	
-		// 4.1 비교하는 함수
-		// !~text.indexOf(val);
-		// 4.2 감추는 함수
-		// filter().hide()
-
-		// var $rows = $('table tr');
-		// $('.search-input').keyup(function() {
-		// 	var val = $.trim($(this).val()).replace(/ + /g, ' ').toLowerCase(); // / + g, ' ' ???
-		// 	$rows.show().filter(function() {
-		// 		var text = $(this).text().replace(/\s + /g, ' ').toLowerCase();
-		// 		return !~text.indexOf(val);	// !~ ??? 
-		// 	}).hide();
-		// });
+		clickEvent = false;
 	});
-})(jQuery);
+});
+
+$(window).load(function() {
+	var boxHeight = $('#blogCarousel .carousel-inner').innerHeight();
+	var itemLength = $('#blogCarousel .item').length;
+	var triggerHeight = Math.round(boxHeight / itemLength + 1);
+
+	$('#blogCarousel .list-group-item').outerHeight(triggerHeight);
+});
